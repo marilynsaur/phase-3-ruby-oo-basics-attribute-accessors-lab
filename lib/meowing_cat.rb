f717482e666f475391e643531cd946a388f7674c
Marilyn Wren <Marilynwren64@yahoo.com>
2022-09-19 14:40:27 -0400

    commit tree 874ea94265de8f58a167ef434c67006e0d2c8527

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,27 @@
 ## code your solution here. 
+class Cat
+    attr_accessor :name
+
+
+    def meow=(meow)
+        @meow = meow
+      end
+    
+      # getter method
+      def meow
+        @meow
+      end
+end
+
+meow = Cat.new
+meow.name = "meow"
+meow.name
+
+# call the Dog#name method
+# puts meow.name
+# "Odie"
+
+# snoopy = Dog.new # => #<Dog:0x007f970a2edfd0>
+# snoopy.breed # => nil
+# snoopy.breed = "Beagle"
+# snoopy.breed # => "Beagle"
